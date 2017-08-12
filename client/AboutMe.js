3c9dfe21b4703400ce617642f94589453f7b50cb
Christian Wentling <christianwentling@gmail.com>
2017-08-12 15:00:55 -0400

    with aboutme component

---
+++ client/AboutMe.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AboutMe (props){
+  return (
+    <div>
+      I am a full stack web developer based in New York.
+    </div>
+  )
+}
--- client/AppComponent.js
+++ client/AppComponent.js
@@ -1,11 +1,15 @@
 import  React, { Component } from 'react';
+import AboutMe from './AboutMe';
 
 export default class AppComponent extends Component {
 
   render () {
     return (
       <div>
-          YOOOO
+        <div className="topName">
+          Christian Wentling
+        </div>
+          <AboutMe />
       </div>
     );
   }
